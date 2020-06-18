8e01e045f28887478cdfbfbd31c9cd5e144cda91
Ian Clark <turbana@gmail.com>
2020-06-17 19:57:16 -0700

    Only use a match's title for the hash

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import hashlib
 
 def get_hash(obj):
     if isinstance(obj, dict):
-        text = obj["title"] + obj["body"]
+        text = obj["title"]
     else:
         text = obj
     return hashlib.md5(text.encode("utf-8")).hexdigest()
