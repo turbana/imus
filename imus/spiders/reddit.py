e0ddf5be8a56ebdb891a2a3892fa67613abb498a
Ian Clark <turbana@gmail.com>
2020-06-28 11:57:18 -0700

    Updated /r/gamedeals blacklist regex

--- imus/spiders/reddit.py
+++ imus/spiders/reddit.py
@@ -38,7 +38,7 @@ class RedditGameDeals(RedditSpider):
     start_urls = ["https://www.reddit.com/r/GameDeals/new/.json"]
     notification_expires = "1w"
     match_regex = re.compile(r'\b(free\b|100%)', re.I)
-    filter_regex = re.compile(r'(gift for reddit|drm[ -]*free|spend over.*get.*free|buy (two|2)( and)? get)', re.I)
+    filter_regex = re.compile(r'(gift for reddit|drm[ -]*free|spend over.*get.*free|buy (one|1|two|2)( and)? get)', re.I)
     title_sub_regex = re.compile(r' {2,}')
     popular_comment_rate = 1.0
     popular_min_age = 30 * 60
